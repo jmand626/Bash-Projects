cf3241d368ffa586578c63b4fccf56b8e35349b4
Joban Mand <86746693+jmand626@users.noreply.github.com>
2026-07-07 20:07:14 -0700

    Add tutorial comments to task3

--- task3.sh
+++ task3.sh
@@ -5,18 +5,25 @@
 # CSE 391 - Autumn 2023
 # Homework 3 - Task 3
 ####################################
+# Tutorial focus:
+# This file moves from single commands to bulk operations. It uses find to
+# discover matching paths, xargs to feed those paths into another command,
+# command substitution to generate arguments, and && / redirection to chain a
+# compile-and-run workflow.
+####################################
 
 function problem6 {
-  # Type your answer to problem #6 below this line
+  # Find directories whose names start with dir and remove them recursively.
   find -type d -name 'dir*' -print | xargs rm -r
 }
 
 function problem7 {
-  # Type your answer to problem #7 below this line
+  # Create directories named 1 through 9 using command substitution.
   mkdir $(seq 1 9)
 }
 
 function problem8 {
-  # Type your answer to problem #8 below this line
-   (ls *.java | xargs javac) && java CheckTransactions 2> err.log
+  # Compile every Java file, then run CheckTransactions if compilation works.
+  # Standard error from the Java program is redirected into err.log.
+  (ls *.java | xargs javac) && java CheckTransactions 2> err.log
 }
